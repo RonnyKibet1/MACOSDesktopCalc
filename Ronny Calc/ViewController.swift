b44cf6ca292825bd21e28acecc3f7e87dbd9a56a
ronnykibet <ronnykibet1@gmail.com>
2018-05-29 20:10:15 -0600

    Initial Commit

---
+++ Ronny Calc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ronny Calc
+//
+//  Created by Ronny Kibet on 5/29/18.
+//  Copyright © 2018 TeamAppCreative. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
